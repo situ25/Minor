f8a5d55febb565237cb138edade900e08d96c38b
AanshiBansal <44017133+AanshiBansal@users.noreply.github.com>
2018-10-11 08:11:27 +0530

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 
 window.onload = function() {
-	var imageScaleFactor = 0.25;
-    var outputStride = 16;
+	var imageScaleFactor = 0.80;
+    var outputStride = 8;
     var flipHorizontal = false;
     var imageElementBack = document.getElementById('back');
 	var imageElementOverlay = document.getElementById('overlay');
@@ -12,20 +12,17 @@ window.onload = function() {
 
     posenet.load().then(function(net){
       return net.estimateSinglePose(imageElementBack, imageScaleFactor, flipHorizontal, outputStride)
-    }).then(function(pose){
+    }).then(function(pose){                      //scope of person body starts here
+		
       console.log(pose);
 	  var x1=pose.keypoints[5].position.x;
 	  var y1=pose.keypoints[5].position.y;
 	  var x2=pose.keypoints[6].position.x;
 	  var y2=pose.keypoints[6].position.y;
-	  xShoulderMid = x2 + (x1-x2)/2;
 	  yShoulderMid = Math.min(y1, y2) + Math.abs(y1 -y2)/2;
-
-	  //console.log(x1-x2);
-	  //console.log(y1);
-	  //console.log(y2);
-	  //console.log(y1-y2);
-	  //!!!get from user
+	  xShoulderMid = x2 + ((x1-x2)/2);
+      console.log(xShoulderMid);
+	  
 	  //var stdDistEyes = 64;     // 64 for men and 62 for female
 	  //var dist_eyes= Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 	  //var scale = stdDistEyes/ dist_eyes;          //stdDistEyes is to be chosen depending upon male or female
@@ -36,12 +33,11 @@ window.onload = function() {
 	 
 	 
 	  //console.log(dist_eyes);
-	  //window.alert(pose);
-    }) 
+
 	
 	posenet.load().then(function(net){
       return net.estimateSinglePose(imageElementOverlay, imageScaleFactor, flipHorizontal, outputStride)
-    }).then(function(pose){
+    }).then(function(pose){                       //scope of t shirt starts here
       console.log(pose);
 	  var x1=pose.keypoints[5].position.x;
 	  var y1=pose.keypoints[5].position.y;
@@ -49,12 +45,9 @@ window.onload = function() {
 	  var y2=pose.keypoints[6].position.y;
 	  xShirtMid = x2 + (x1-x2)/2;
 	  yShirtMid = Math.min(y1, y2) + Math.abs(y1 -y2)/2;
-	  console.log(xShoulderMid);
-    }) 
-	
-	
+	  console.log(xShirtMid);
     
-    var mainImageWidth=document.getElementById("back").width;                  //###Set this accordingly in HTML
+	var mainImageWidth=document.getElementById("back").width;                  //###Set this accordingly in HTML
     var mainImageHeight=document.getElementById("back").height;                   //###Set this accordingly in HTML
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
@@ -65,5 +58,13 @@ window.onload = function() {
 	console.log(overlayPositionX);
     var overlayPositionY= yShoulderMid - yShirtMid;    //#### find using formulae
     ctx.drawImage(overlayImg, overlayPositionX,overlayPositionY,document.getElementById("overlay").width,document.getElementById("overlay").height);           //the last two 50 to be replaced with image sizes
-}
+	                                                         
+															 //end of scope of tshirt 
+															 })
+ 															 //end of scope of persdon body
+															 })
+	
+	
     
+    
+}
